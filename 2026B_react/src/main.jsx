3224df8335c306fab46dd098541ca1d559a7ce16
seungy07 <ksykim07@gmail.com>
2026-09-13 20:39:11 +0900

    일단 리액트 1일치 모두 코드 작성

--- 2026B_react/src/main.jsx
+++ 2026B_react/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 //[필수] 1. 리액트 라이브러리 최초 렌더링(그리기)하는 함수
 import { createRoot } from 'react-dom/client'
+//[필수] 2. index.html 에서 root 마크업 가져오기 , #ID , .Class
+const root = document.querySelector( '#root' )
+//[필수] 3. 가져온 root 마크업을 createRoot 함수에 전달한다.
+const create = createRoot( root );
 // [선택] 최초로 화면을 그리기 할 컴포넌트 가져와서 렌더링
 // 1. import 이용하여 컴포넌트 가져온다. 
 // import App from './App.jsx'
