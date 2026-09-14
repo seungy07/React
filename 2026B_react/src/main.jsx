89cc1c3bdc90a542f44a2a789ac67e4357f6b8e6
seungy07 <ksykim07@gmail.com>
2026-09-14 09:33:44 +0900

    react day02 수업 시작전

--- 2026B_react/src/main.jsx
+++ 2026B_react/src/main.jsx
@@ -27,5 +27,7 @@ const create = createRoot( root );
 // import Component4 from "./example/day01/Component4";
 // create.render( <Component4/> )
 
-import Component4 from "./example/day47_day01_0911/Component4";
-create.render( <Component4/> )
+import Component5 from "./example/day47_day01_0911/Component5";
+create.render( <Component5/> )
+
+// [day02] 
